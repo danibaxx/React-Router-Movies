6b56c8acce53e77bf4daf6d8739ae59c1ba12077
Danielle B <danibaxx@hotmail.com>
2019-06-23 19:19:43 -0600

    moved info for MovieCard from MovieList.js into MovieCard.js and made into a Link

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -10,8 +10,9 @@ export default class Movie extends Component {
   }
 
   componentDidMount() {
-    // change this line to grab the id passed on the URL
-    const id = 1;
+    console.log(this.props.match.params.id);
+    const id = this.props.match.params.id;
+    // const id = 1;
     this.fetchMovie(id);
   }
 
